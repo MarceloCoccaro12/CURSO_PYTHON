9c3b7a128baccab87c19c9a6c2a043d513d0dbf6
MarceloCoccaro12 <162065215+MarceloCoccaro12@users.noreply.github.com>
2025-01-06 18:16:04 -0300

    0601

---
+++ aula156/uma_linha.py
@@ -0,0 +1,7 @@
+"""O que seu do módulo faz"""
+
+
+variavel = 'valor'
+
+def funcao():
+    return 1
---
+++ aula156/varias_linhas.py
@@ -0,0 +1,18 @@
+"""O que sseu módulo faz
+
+Um cidadão fez voto de desapego e pobreza. Dispôs de todos os seus bens e 
+propriedades, reservou para si apenas duas 
+tangas, e saiu pelo mundo afora em busca de todos os sábios, medindo na verdade 
+o desapego de cada um. Levava apenas uma 
+tanga no corpo.
+Estava convencido de não encontrar quem ganhasse de si em despojamento, quando 
+soube de um velho guru. Tomando as 
+direções, parte ao encontro do velho sábio.
+Quando lá chegou, tristeza e decepção! Encontrou terras bem cuidadas, um palácio 
+faustoso, muita riqueza, muita pompa. Indignado, procura pelo guru. Um velho 
+servo lhe diz que ele está em uma ala dos magníficos jardins com seus 
+discípulos, estudando desapego. Como era costume da casa Ter gentileza para com 
+os hóspedes, o servo convida o andarilho para o banho, repouso e refeição, antes 
+de se dirigir à presença do sábio.
+
+"""
